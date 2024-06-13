352c82ae27c398cff91266a5b0ae3c39e25482a4
Riccardo Mereu <r.mereu@arduino.cc>
2024-06-13 15:11:04 +0200

    [COMMON] Common header file with defines and error typedef.

---
+++ common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+
+#define LOGIN_TIMEOUT 60000
+#define COMMAND_TIMEOUT 10000
+#define LOGIN_STRING "login:"
+
+#define PSW_STRING "fio"
+#define USR_STRING "fio"
+
+#define PSW_PROMPT "Password:"
+#define IN_SHELL_PROMPT "@dev-portenta"
+
+typedef enum {
+    ERROR_INIT_FAILED,
+    ERROR_TIMEOUT,
+    ERROR_INVALID_STATE,
+    ERROR_INVALID_COMMAND,
+    ERROR_INVALID_ARGUMENT,
+    ERROR_INVALID_RESPONSE,
+    NO_ERROR = 99
+} TesterError;
