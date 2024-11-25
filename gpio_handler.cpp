2d2f1398fb1527fc8900712576da776b0778a896
Riccardo Mereu <r.mereu@arduino.cc>
2024-11-25 10:21:16 +0100

    [GPIO] Fixing mixed use of DAC channel

--- gpio_handler.cpp
+++ gpio_handler.cpp
@@ -19,6 +19,28 @@ TesterError GpioHandler::setGpioStatus(int gpio, int value) {
     TesterError opStatus = NO_ERROR;
 
     pinMode(gpio, OUTPUT);
+#ifdef DAC_0 || DAC
+    if (gpio == DAC_0 || gpio == DAC) {
+        if(value == 1)
+            analogWrite(gpio, 255);
+        else{
+            analogWrite(gpio, 0);
+        }
+        return opStatus;
+    }
+#endif
+
+#ifdef DAC_1
+    if (gpio == DAC_1) {
+        if(value == 1)
+            analogWrite(gpio, 255);
+        else{
+            analogWrite(gpio, 0);
+        }
+        return opStatus;
+    }
+#endif
+
     digitalWrite(gpio, value);
 
     return opStatus;
