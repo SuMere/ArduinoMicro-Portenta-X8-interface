f152386f77d8e00be186a34fb04c18d49d96fae8
Luca Burelli <l.burelli@arduino.cc>
2024-12-05 10:47:43 +0100

    wip i2c e uart

--- uart_handler.h
+++ uart_handler.h
@@ -7,8 +7,8 @@
 
 class UartHandler : public CmdHandler {
     private:
-        bool is_confgured[UART_COUNT] = {false};
-        
+        bool is_configured[UART_COUNT] = {false};
+
         virtual chAT::CommandStatus handle_read(chAT::Server&, chAT::ATParser&);
         virtual chAT::CommandStatus handle_write(chAT::Server&, chAT::ATParser&);
         virtual chAT::CommandStatus handle_cfg_read(chAT::Server&, chAT::ATParser&);
@@ -17,7 +17,6 @@ class UartHandler : public CmdHandler {
         int send_message(int uart_number, String message);
         int receive_message(int uart_number, String *message);
         int set_configuration(int uart_number, int data_bits, int stop_bits, String parity, int baud_rate);
-        bool is_configured(int uart_number);
 
     public:
         UartHandler() : CmdHandler("+UART") {}
